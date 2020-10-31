2ba7cb72a4804af7e2b447afad5239cd487c4551
jiangyi01 <3197797064@qq.com>
2020-10-31 16:27:42 +0800

    yidian

---
+++ src/mscIndex.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class MscIndex extends Component{
+    render(){
+        return (
+            <div>
+            这是一个mscSdu的门户网站
+            </div>
+        )
+    }
+} 
+export default MscIndex;
