2bad495b574c333a70b9bdfcbefcdd7476fc733e
jiangyi01 <3197797064@qq.com>
2021-01-18 10:26:42 +0800

    关于调试错误

--- src/redux/login.js
+++ src/redux/login.js
@@ -6,59 +6,59 @@ import {
     LOGIN_SUCCESS
 } from "../login/login_redux";
 const defaultState = {
-    login_in = false,
-    login_off = true,
-    login_fail = 0,
-    login_success = 0,
-    name = '',
-    id = '',
-    token = ''
+    login_in : false,
+    login_off : true,
+    login_fail : 0,
+    login_success : 0,
+    name : '',
+    id : '',
+    token : ''
 };
 
 export default function login(state = defaultState, action){
     switch(action.type){
-
+        
         case GET_MESSAGE_OFYOU:
             return{
                 ...state,
-                name = action.payload.name,
-                id = action.payload.id,
-                token = action.payload.token
+                name : action.payload.name,
+                id : action.payload.id,
+                token : action.payload.token
             }
         case LOGIN_IN:
             return {
                 ...state,
-                login_in = true,
-                login_off = false,
-                login_fail = 2,
-                login_success = 2
+                login_in : true,
+                login_off : false,
+                login_fail : 2,
+                login_success : 2
 
         }
         case LOGIN_OFF:
             return{
                 ...state,
-                login_in = false,
-                login_off = true,
-                login_fail = 0,
-                login_success = 0
+                login_in : false,
+                login_off : true,
+                login_fail : 0,
+                login_success : 0
         }
         
         case LOGIN_SUCCESS:
             return {
                 ...state,
-                login_in = true,
-                login_off = false,
-                login_fail = 0,
-                login_success = 1
+                login_in : true,
+                login_off : false,
+                login_fail : 0,
+                login_success : 1
         }
         
         case LOGIN_FAIL:
             return{
                 ...state,
-                login_in = false,
-                login_off = true,
-                login_fail = 1,
-                login_success = 0
+                login_in :false,
+                login_off : true,
+                login_fail : 1,
+                login_success : 0
         }
         
         default:
